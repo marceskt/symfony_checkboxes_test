cee887e65f10da819142e9ce785fa7b6c03bd9ac
Marcelino Hernandez Lopez <marce.skt@gmail.com>
2022-02-09 02:19:25 -0600

    Almacenar perfil nuevo

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Permission;
 use App\Entity\Profile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -43,6 +44,36 @@ class ProfileController extends AbstractController
 				'permissions' => $permissions
 			)
 		);
+	}
+
+	/**
+	 * @Route(path="/create", methods={"POST"}, name="post.profiles.create")
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function postProfile(Request $request): Response
+	{
+		/** @var string $name */
+		$name = $request->request->get('name');
+		/** @var integer[] $permissionsId */
+		$permissionsId = $request->request->get('permissions') ?? array();
+
+		/** @var Permission[] $permissions */
+		$permissions = $this->getDoctrine()->getRepository(Permission::class)
+			->findBy(array( 'id' => $permissionsId ));
+
+		$profile = new Profile();
+
+		$profile->setName($name);
+
+		foreach ($permissions as $permission) {
+			$profile->addPermission($permission);
+		}
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($profile);
+		$em->flush();
+
+		return $this->redirectToRoute('get.profiles.listview');
 	}
 
 }
